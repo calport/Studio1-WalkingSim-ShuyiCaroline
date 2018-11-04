10609a06a01d840574e0c5c1ca0dcdc50348a9de
calport <cap540@nyu.edu>
2018-11-04 17:41:51 -0500

    All sorts of particle effects and stuff from online. This is the commit after the disaster of writing over my own files. Sorry its huge.

---
+++ Studio1_WalkingSim/Assets/Scripts/AudioBetweenPlayers.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioBetweenPlayers : MonoBehaviour
+{
+	private float PlayerDistanceX;
+	private float PlayerDistanceY;
+	private float PlayerDistanceZ;
+
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		PlayerDistanceX = (GameObject.Find("FPSController_1").transform.position.x +
+		                  GameObject.Find("FPSController_2").transform.position.x) / 2f;
+		PlayerDistanceY = (GameObject.Find("FPSController_1").transform.position.y +
+		                  GameObject.Find("FPSController_2").transform.position.y) / 2f;
+		PlayerDistanceZ = (GameObject.Find("FPSController_1").transform.position.z +
+		                  GameObject.Find("FPSController_2").transform.position.z) / 2f;
+
+		this.transform.position = new Vector3(PlayerDistanceX, PlayerDistanceY, PlayerDistanceZ);
+	}
+
+	
+}
