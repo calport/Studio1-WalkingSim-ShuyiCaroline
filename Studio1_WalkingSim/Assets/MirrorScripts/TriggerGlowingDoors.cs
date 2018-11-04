f1829fd976b41d5cbcddd88030b3b5c72e62e517
shuyi <mycalendula@gmail.com>
2018-11-04 14:47:03 -0500

    Added package that will make cool particle effects on our player characters. But I haven't figured out to make a skinned mesh of a walking person yet.

---
+++ Studio1_WalkingSim/Assets/MirrorScripts/TriggerGlowingDoors.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = System.Object;
+
+public class TriggerGlowingDoors : MonoBehaviour
+{
+
+
+	public Animator myanim;
+	
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//myanim.Play();
+		
+		
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		print("true1");
+		
+		
+		if (other.gameObject.CompareTag("Player"))
+		{
+			print("true2");
+			myanim.SetBool("TRIGGERED",true);
+			
+			
+		}
+
+		//if (other.gameObject.CompareTag("Player"))
+		//{
+
+		//	Boolean DoorsGlow = true;
+		//	print("true");
+		//}
+		
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+
+			//Boolean DoorsGlow = false;
+			print("false");
+			myanim.SetBool("TRIGGERED",false);
+		}
+	}
+}
